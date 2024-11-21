485c8a57e3b98f45af2df3ae47ac5ad950cf622e
Gabriel Marangoni <88441780+marang4@users.noreply.github.com>
2024-11-21 20:16:20 -0300

    Delete clientes-backend-api directory

--- clientes-backend-api/src/main/java/br/com/senac/api/repositorios/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.senac.api.repositorios;
-
-import br.com.senac.api.entidades.Endereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-}
