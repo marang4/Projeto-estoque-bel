66dc173745146a667d3f2686fc2663489e46822a
Gabriel Marangoni <88441780+marang4@users.noreply.github.com>
2024-11-21 20:16:29 -0300

    Delete api-clientes directory

--- api-clientes/clientes-backend-api/src/main/java/br/com/senac/api/repositorios/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.senac.api.repositorios;
-
-import br.com.senac.api.entidades.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
