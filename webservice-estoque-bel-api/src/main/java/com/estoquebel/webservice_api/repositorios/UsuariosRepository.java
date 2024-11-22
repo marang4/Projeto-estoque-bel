d6a1ceb398fba8decfcfe1bc21bb1eba3344bc0c
Gabriel Marangoni <88441780+marang4@users.noreply.github.com>
2024-11-21 21:41:13 -0300

    Delete webservice-estoque-bel-api directory

--- webservice-estoque-bel-api/src/main/java/com/estoquebel/webservice_api/repositorios/UsuariosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.estoquebel.webservice_api.repositorios;
-
-import com.estoquebel.webservice_api.entidades.Usuarios;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
-    Usuarios findByEmail(String email);
-}
